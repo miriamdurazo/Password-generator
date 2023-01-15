817f11da16ded3add7387e4d12dec2742765c913
miriamdurazo <md13707@my.bristol.ac.uk>
2023-01-15 18:13:23 +0000

    added an if statement to the getPasswordOptions function so that users' input does not include letters

--- script.js
+++ script.js
@@ -90,13 +90,20 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-  let pwdLen = prompt("Choose how long you password will be: type a number from 10 to 64");
+  let pwdLen = prompt("Choose the length of your password: enter a number from 10 to 64");
+  if  (/[a-zA-Z]/.test(pwdLen)) {
+    alert("Sorry, that is not a valid number");
+    pwdLen = prompt("Please enter number from 10 to 64");
+  } 
+
   let pwdLenNum = parseInt(pwdLen);
   if (pwdLenNum < 10 || pwdLenNum > 64) {
     alert("The number you chose is out of range")
   }
+
   let charsTypes = ["lowercase", "uppercase", "numeric", "special ($@%&*, etc)"];
   let userChoices = { passwordLength: pwdLenNum};
+  
   for (const characterType in charsTypes) {
     userChoices[charsTypes[characterType]] = confirm(`Do you want your password to include ${charsTypes[characterType]} characters?`);
     // console.log(userChoices);  
